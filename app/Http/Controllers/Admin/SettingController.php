84cdc0d7d30baaf5e3ddd22b1344f56b3103efad
AB-consulting <89553342+scorpion237@users.noreply.github.com>
2026-06-22 13:39:42 +0100

    setting update

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -10,6 +10,26 @@ class SettingController extends Controller
 {
     public function index()
     {
+         $defaults = [
+        ['key' => 'site_title',       'value' => 'ONG ADH', 'group' => 'general'],
+        ['key' => 'donation_url',     'value' => '#faire-un-don-modal', 'group' => 'general'],
+        ['key' => 'site_description', 'value' => '', 'group' => 'seo'],
+        ['key' => 'google_analytics', 'value' => '', 'group' => 'analytics'],
+        ['key' => 'contact_email',    'value' => '', 'group' => 'contact'],
+        ['key' => 'contact_phone',    'value' => '', 'group' => 'contact'],
+        ['key' => 'contact_address',  'value' => '', 'group' => 'contact'],
+        ['key' => 'facebook_url',     'value' => '', 'group' => 'contact'],
+        ['key' => 'twitter_url',      'value' => '', 'group' => 'contact'],
+        ['key' => 'linkedin_url',     'value' => '', 'group' => 'contact'],
+        ['key' => 'whatsapp_number',  'value' => '', 'group' => 'contact'],
+    ];
+
+    foreach ($defaults as $default) {
+        \App\Models\Setting::firstOrCreate(
+            ['key' => $default['key']],
+            $default
+        );
+    }
         $settings = Setting::all()->groupBy('group');
         return view('admin.settings.index', compact('settings'));
     }
